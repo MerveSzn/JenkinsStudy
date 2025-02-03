21cabf740d80f10eb656eaff45acd929b1391bfa
merve.sozen <merve.sozen@tubitak.gov.tr>
2025-02-03 12:59:18 +0300

    İlk Proje

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,13 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     // Configure your E2E tests here
-    specPattern: "cypress/e2e/**/*.{cy,spec}.{js,ts}"
-  },
-})
+    specPattern: "cypress/e2e/**/*.{cy,spec}.{js,ts}",
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: "cypress/results",
+      overwrite: false,
+      html: true,
+      json: true
+    }
+  }
+});
